69c727abdc48aa1bc5d29e735405e7f8e119a73a
Mike Houghton <mike_k_houghton@yahoo.co.uk>
2019-12-01 09:52:03 +0000

    Created.

---
+++ src/aoc2019/Day1.kt
@@ -0,0 +1,33 @@
+package aoc2019
+
+import java.io.File
+
+fun readFileLinesAsInt(fname: String): List<Int> =
+    readFileLines(fname).map { it.toInt() }
+
+fun readFileLines(fname: String): List<String> {
+    val input = mutableListOf<String>()
+    File(fname).useLines { all -> input.addAll(all) }
+    return input
+}
+
+fun basicFuelCalc(x: Int): Int = x.div(3) - 2
+
+fun calcFuel(m: Int): Int {
+    fun totalFuel(x: Int, acc: Int): Int {
+        val fuel = basicFuelCalc(x)
+        if (fuel <= 0) return acc
+        return totalFuel(fuel, fuel + acc)
+    }
+    return totalFuel(m, 0)
+}
+
+fun main() {
+
+    val numbers = readFileLinesAsInt("resources/Day1.txt")
+    println("Part 1")
+    println(numbers.map { basicFuelCalc(it) }.sum())
+
+    println("Part 2")
+    println(numbers.map { calcFuel(it) }.sum())
+}
