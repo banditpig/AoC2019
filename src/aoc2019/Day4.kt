87503e59959b68cd14bf10272fed7bcb3f73d15c
Mike Houghton <mike_k_houghton@yahoo.co.uk>
2019-12-04 17:38:28 +0000

    Day4 part2 and refactor.

--- src/aoc2019/Day4.kt
+++ src/aoc2019/Day4.kt
@@ -8,12 +8,32 @@ fun hasPair(num:String): Boolean =
     (0 .. num.length - 2).fold(false){ac, ix ->
         ac || num[ix] == num[ix + 1]}
 
-fun check(num:String ): Boolean = increasing(num) && hasPair(num)
+fun hasPairStrict(num:String): Boolean {
+            fun next(ix: Int) : Boolean {
+                if (ix >= num.length  - 1) return true
+                return num[ix] != num[ix + 1]
+            }
+            fun prev(ix: Int) : Boolean {
+                if (ix == 0 ) return true
+                return  (num[ix] != num[ix - 1])
 
+            }
+       return (0..num.length - 2).fold(false) { ac, ix ->
+            ac || ( num[ix] == num[ix + 1]) && (next(ix + 1) && prev(ix))
+        }
+}
+
+fun check(num:String, pairOk:(String) -> Boolean):Boolean{
+    return increasing(num) && pairOk(num)
+}
 fun main() {
 
     println("Part 1")
-    var count = (245182..790572).filter(){n -> check(n.toString())}.count()
+    var count = (245182..790572).filter(){n -> check(n.toString(),::hasPair)}.count()
+    println(count)
+
+    println("Part 1")
+    count = (245182..790572).filter(){n -> check(n.toString(),::hasPairStrict)}.count()
     println(count)
 }
 
